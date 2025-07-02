1e588f6ce001f285897e7d2613dcb595e2b1b410
Imre Tabur <imre.tabur@mail.ee>
2025-07-02 18:42:18 +0300

    Save work

--- src/test/js/application.e2e.js
+++ src/test/js/application.e2e.js
@@ -1,9 +1,9 @@
 const {test, expect} = require('@playwright/test');
 const pageHelper = require('./pageHelper');
 
-const pageName = 'main';
+const pageName = 'application';
 
-test.describe('Main page layout tests', async () => {
+test.describe('application page layout tests', async () => {
 
     test.beforeAll(async () => {
         pageHelper.pageName(pageName);
@@ -15,7 +15,7 @@ test.describe('Main page layout tests', async () => {
     test('should load the page and check title', async () => {
 
         const title = await pageHelper.data.page.title();
-        expect(title).toBe('main.html');
+        expect(title).toBe('application.html');
 
         await pageHelper.elementExpectations(
             'body',
--- src/test/js/pugHelper.js
+++ src/test/js/pugHelper.js
@@ -2,6 +2,19 @@ const pug = require('pug');
 const fs = require('fs');
 const path = require('path');
 
+const pageNames = [
+    "body",
+    "app",
+    "application"
+];
+
+function main() {
+    for (let i = 0; i < pageNames.length; i++) {
+        let pageName = pageNames[i];
+        compilePug(pageName, {title: pageName + ".html"});
+    }
+}
+
 function compilePug(name, locals = {}) {
     const pugFile = path.join(__dirname, '../pug/' + name + ".pug");
     const htmlFile = path.join(__dirname, '../../../dist/' + name + ".html");
@@ -9,6 +22,8 @@ function compilePug(name, locals = {}) {
     fs.writeFileSync(htmlFile, compiledFunction(locals));
     console.log('Pug template compiled to HTML successfully.');
 }
+
+main();
 
 module.exports = {
     compilePug

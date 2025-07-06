e801aada4f68ee5ccf88af2d048e2f4379e10250
Imre Tabur <imre.tabur@mail.ee>
2025-07-06 11:37:31 +0300

    Refactored partials build

--- src/test/js/pugBuild.js
+++ src/test/js/pugBuild.js
@@ -3,7 +3,13 @@ const path = require('path');
 const pugTranspile = require('./pugTranspile');
 
 const inputDir = path.join(__dirname, '../pug');
-const partials = ["abc"];
+const partialsDir = path.join(inputDir, 'partials');
+
+function getPartials() {
+    return fs.readdirSync(partialsDir)
+        .filter(file => file.endsWith('.pug'))
+        .map(file => path.basename(file, '.pug'));
+}
 
 function main() {
     const dir = inputDir;
@@ -14,6 +20,7 @@ function main() {
         if (stat.isFile() && file.endsWith('.pug')) {
             const pageName = path.basename(filePath, path.extname(filePath));
             if (pageName === "main") {
+                const partials = getPartials();
                 for (const partial of partials) {
                     pugTranspile.transpileHtml(pageName, {
                         title: partial + '.html',
